2fb78efd263c88aeba997df7dd13cae4de26d0a2
Luís Miguel Silva Amorim <luismiguelsilvaamorim@gmail.com>
2022-01-05 08:39:07 -0300

    fixing issues

---
+++ helpers/counter.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+
+def counter(time):
+    for c in range(0, int(time)):
+        print(c)
+        sleep(1)
+    return
--- helpers/sleep.py
+++ helpers/sleep.py
@@ -1,12 +1,11 @@
-from time import sleep
-
 import helpers.time_conversor
 import helpers.menus
+import helpers.counter
 
 
 def set_sleep_timer():
     time_unit = helpers.menus.time_unit()
     time_to_action = helpers.menus.timer()
     time = helpers.time_conversor.to_seconds_conversor(time_unit, time_to_action)
-    sleep(time)
+    helpers.counter.counter(time)
     return
